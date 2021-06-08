fa3d4e4d50f4fec23d271e01dd63fa045db1e6e4
sudipulak <sdpulak123@gmail.com>
2021-06-08 22:37:12 +0545

    script cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,7 @@
-import MobileMenu from "modules/mobileMenu.js";
-
-// Instantiate a new object using our modules/classes
-var mobileMenu = new MobileMenu();
-
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  //autoplay:
-  //delay: 5000,
-  //disableOnInteraction: false,
-  //},
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
