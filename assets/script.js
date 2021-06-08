0fdf9dc2bea1d4cc7cb7adceef54a2ddca6a81d4
sudipulak <sdpulak123@gmail.com>
2021-06-09 00:32:16 +0545

    responsive navigation initial

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
+// Navigation Menu
+var trigger = document.querySelector("#hamburger");
+var navigation = document.querySelector(".main-navigation");
+
+trigger.addEventListener("click", function () {
+  navigation.classList.toggle("show");
+});
+
+console.log(trigger.classList);
+
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
+  //autoplay:
+  //delay: 5000,
+  //disableOnInteraction: false,
+  //},
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
